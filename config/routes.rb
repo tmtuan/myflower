8223b0763ca2dec196f488d811834ef34a027907
tmtuan <tmtuan.projects@gmail.com>
2015-10-29 23:04:24 +0700

    Adds CRUD factors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'factors/index'
+
   get 'index' => 'statements#index'
   post 'score' => 'statements#score'
   get 'list' => 'statements#list'
 
   resources :statements
+  resources :factors
 end
