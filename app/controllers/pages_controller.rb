788cb885301cb0c9177a05a650bc505646abf1a1
tmtuan <tmtuan.projects@gmail.com>
2015-11-11 20:39:05 +0700

    Adds steps to pages_controllers, adds links to myflower to go corresponding pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,33 @@ class PagesController < ApplicationController
   def home
     render layout: "layout_landingpage"
   end
+
+  def values
+
+  end
+
+  def knowledgeExpertise
+
+  end
+
+  def transferableSkills
+
+  end
+
+  def people
+
+  end
+
+  def workingConditions
+
+  end
+
+  def salary
+
+  end
+
+  def place
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,14 @@ Rails.application.routes.draw do
   resources :statements
   resources :factors
 
+  #This is url for 'pages'
   get 'home' => 'pages#home'
+  get 'step1_values' => 'pages#values'
+  get 'step2_knowledge_expertise' => 'pages#knowledgeExpertise'
+  get 'step3_transferableskills' => 'pages#transferableSkills'
+  get 'step4_people' => 'pages#people'
+  get 'step5_workingconditions' => 'pages#workingConditions'
+  get 'step6_salary' => 'pages#salary'
+  get 'step7_place' => 'pages#place'
+
 end
